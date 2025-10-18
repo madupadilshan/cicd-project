025967f629a2f7101628b12b5c4975a4f0c24edc
Madupa Dilshan <madupadilshan111@gmail.com>
2025-10-18 17:42:21 +0530

    Test V2.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Wow! My CI/CD Pipeline is Working! V1.0');
+  res.send('Work! My CI/CD Pipeline is Working! V1.0');
 });
 
 app.listen(PORT, () => {
